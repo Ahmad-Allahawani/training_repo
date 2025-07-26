c76cb59463a8c33fe95176713b1ab562794bcef3
Ahmad-Allahawani <ahmad.allahawani04@gmail.com>
2025-07-26 21:21:06 +0300

    test

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,4 @@
-const isGithubPages = process.env.GITHUB_PAGES === 'true';
-
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
- 
-};
+const nextConfig = {};
 
-// module.exports = nextConfig;
 export default nextConfig;
