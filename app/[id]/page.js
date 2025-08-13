15465a900fe2b8c82827033e0be6ca7eda567897
Ahmad-Allahawani <ahmad.allahawani04@gmail.com>
2025-08-14 01:53:31 +0300

    fixed_params_problem_in_[id]page

--- app/[id]/page.js
+++ app/[id]/page.js
@@ -1,20 +1,23 @@
 "use client";
 import { useState, useEffect } from "react";
-import { notFound } from "next/navigation";
+import { useParams, notFound } from "next/navigation";
 
 export default function Page({ params }) {
   const [data, setData] = useState(null);
   const [showSecond, setShowSecond] = useState(true);
+  const { id } = useParams();
 
   useEffect(() => {
+   
     async function fetchData() {
-      const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/text/${params.id}`);
+      
+      const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/text/${id}`);
       if (!res.ok) return notFound();
       const json = await res.json();
       setData(json);
     }
     fetchData();
-  }, [params.id]);
+  }, [id]);
 
   if (!data) return <p>Loading...</p>;
 
@@ -22,7 +25,7 @@ export default function Page({ params }) {
     <main className="min-h-screen bg-gray-100 flex items-center justify-center p-4 gap-4">
       <div className="w-full max-w-2xl bg-white shadow-lg rounded-lg p-6">
         <div className="flex justify-between items-center mb-6">
-          <h1 className="text-3xl font-bold text-gray-800">Shared  Text </h1>
+          <h1 className="text-3xl font-bold text-gray-800">Shared Text </h1>
           <button
             onClick={() => setShowSecond(!showSecond)}
             className="px-3 py-1 bg-blue-500 text-white rounded hover:bg-blue-600"
